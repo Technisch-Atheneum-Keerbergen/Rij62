8e05188f1b5ac5c199a1ef13cb4bcff11576e64a
Localhost2011 <xavier.demaerel@hotmail.com>
2026-02-23 14:49:43 +0100

    Making aspire work with database and c#

--- Rij62.AppHost/Program.cs
+++ Rij62.AppHost/Program.cs
@@ -1,3 +1,11 @@
+using Aspire;
+using Aspire.Hosting;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.Build().Run();
+// Use a friendly ASCII-only resource name
+builder.AddPostgres("rij62-db");
+
+var app = builder.Build();
+
+app.Run();
