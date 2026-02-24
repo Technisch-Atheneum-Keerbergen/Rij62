3eef5bb5aa452aa3b12115781bd90c8380d6257f
Localhost2011 <xavier.demaerel@hotmail.com>
2026-02-24 13:26:40 +0100

    Added simple crud

--- Rij62/Controllers/ProductController.cs
+++ Rij62/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Rij62.Data;
+using Rij62.Models;
 
 namespace Rij62.Controllers
 {
@@ -15,11 +16,51 @@ namespace Rij62.Controllers
             _context = context;
         }
 
+        // Get all products
         [HttpGet]
         public async Task<IActionResult> GetProducts()
         {
             var products = await _context.Products.ToListAsync();
             return Ok(products);
+        }
+
+        // Get product by id
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null) return NotFound();
+            return Ok(product);
+        }
+
+        // Create a new product
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct([FromBody] Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
+        }
+
+        // Update a product
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProduct(int id, [FromBody] Product updatedProduct)
+        {
+            if (id != updatedProduct.Id) return BadRequest();
+
+            var product = await _context.Products.FindAsync(id);
+            if (product == null) return NotFound();
+
+            product.TitleKey = updatedProduct.TitleKey;
+            product.DescriptionKey = updatedProduct.DescriptionKey;
+            product.PriceCent = updatedProduct.PriceCent;
+            product.Stock = updatedProduct.Stock;
+            product.IsAvailable = updatedProduct.IsAvailable;
+            product.ImgUrl = updatedProduct.ImgUrl;
+            product.CategoryId = updatedProduct.CategoryId;
+
+            await _context.SaveChangesAsync();
+            return NoContent();
         }
     }
 }
