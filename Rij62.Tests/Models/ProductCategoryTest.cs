3d3aac5445941ec489e5a753d29f17bde33d24b5
Localhost2011 <xavier.demaerel@hotmail.com>
2026-03-09 10:47:18 +0100

    Test file for Table Controller "qoui pa pa pada paaa" -zaz

--- Rij62.Tests/Models/ProductCategoryTest.cs
+++ Rij62.Tests/Models/ProductCategoryTest.cs
@@ -1,34 +1,111 @@
 // **********************************
-//     *** ProductCategory Model Test  ***
+//     *** Table Controller Test  ***
 // Author: Xavier Demaerel
-// Date: 03/03/2026
-// File: Rij62.Tests\Models\ProductCategoryTest.cs
+// Date: 09/03/2026
+// File: Rij62.Tests\Controllers\TableControllerTest.cs
 // **********************************
 
-
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Rij62.Controllers;
+using Rij62.Data;
 using Rij62.Models;
 using Xunit;
 
-namespace Rij62.Tests.Models
+namespace Rij62.Tests.Controllers
 {
-    public class ProductCategoryTest
+    public class TableControllerTest
     {
+        private AppDbContext GetDbContext()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDb")
+                .Options;
+
+            return new AppDbContext(options);
+        }
+
         [Fact]
-        public void CanCreateProductCategory()
+        public async Task CanCreateTable()
         {
             // Arrange
-            var productCategory = new ProductCategory
+            var context = GetDbContext();
+            var controller = new TableController(context);
+
+            var table = new Table
             {
-                Id = 1,
-                ScreenId = 2,
-                Name = "Test Category"
+                TableNumber = 10
             };
 
-            // Act & Assert
-            Assert.NotNull(productCategory);
-            Assert.Equal(1, productCategory.Id);
-            Assert.Equal(2, productCategory.ScreenId);
-            Assert.Equal("Test Category", productCategory.Name);
+            // Act
+            var result = await controller.CreateTable(table);
+
+            // Assert
+            var actionResult = Assert.IsType<ActionResult<Table>>(result);
+            var createdResult = Assert.IsType<CreatedAtActionResult>(actionResult.Result);
+
+            var createdTable = Assert.IsType<Table>(createdResult.Value);
+
+            Assert.Equal(10, createdTable.TableNumber);
+        }
+
+        [Fact]
+        public async Task GetTablesReturnsTables()
+        {
+            // Arrange
+            var context = GetDbContext();
+            context.Tables.Add(new Table { TableNumber = 1 });
+            context.Tables.Add(new Table { TableNumber = 2 });
+            context.SaveChanges();
+
+            var controller = new TableController(context);
+
+            // Act
+            var result = await controller.GetTables();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result.Result);
+            var tables = Assert.IsAssignableFrom<IEnumerable<Table>>(okResult.Value);
+
+            Assert.Equal(2, tables.Count());
+        }
+
+        [Fact]
+        public async Task GetTableReturnsSingleTable()
+        {
+            // Arrange
+            var context = GetDbContext();
+            context.Tables.Add(new Table { Id = 1, TableNumber = 5 });
+            context.SaveChanges();
+
+            var controller = new TableController(context);
+
+            // Act
+            var result = await controller.GetTable(1);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result.Result);
+            var table = Assert.IsType<Table>(okResult.Value);
+
+            Assert.Equal(5, table.TableNumber);
+        }
+
+        [Fact]
+        public async Task DeleteTableRemovesTable()
+        {
+            // Arrange
+            var context = GetDbContext();
+            context.Tables.Add(new Table { Id = 1, TableNumber = 3 });
+            context.SaveChanges();
+
+            var controller = new TableController(context);
+
+            // Act
+            var result = await controller.DeleteTable(1);
+
+            // Assert
+            Assert.IsType<NoContentResult>(result);
+            Assert.Empty(context.Tables);
         }
     }
 }
