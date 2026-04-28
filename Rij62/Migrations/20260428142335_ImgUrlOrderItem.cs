50e54e3f9df86de900662ab1d7b1c832320ed5af
ldev <ldev@ldev.eu.org>
2026-04-28 16:23:51 +0200

    forgor to add the imgurl migration

---
+++ Rij62/Migrations/20260428142335_ImgUrlOrderItem.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rij62.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImgUrlOrderItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "img_url",
+                table: "order_items",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "img_url",
+                table: "order_items");
+        }
+    }
+}
